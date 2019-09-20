6e10e128b0776939f5874edb43c272f71df51f54
donatKapesa <donatkap14@gmail.com>
2019-09-19 23:09:24 -0400

    discarded some changes. Added some changes to sign in page

--- src/containers/SignInPage/SignInPage.js
+++ src/containers/SignInPage/SignInPage.js
@@ -13,8 +13,56 @@ class SignInPage extends Component {
   render() {
     console.log(window);
     return (
-      <div className="SignInPage">
-        <button onClick={this.signInHandler}>Sign in with Spotify</button>
+      <div class="header">
+        <div class="inner-header flex">
+          <a
+            onClick={this.signInHandler}
+            class="waves-effect waves-light btn sign-in-button"
+          >
+            Sign in with Spotify
+          </a>
+        </div>
+        <div>
+          <svg
+            class="waves"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 24 150 28"
+            preserveAspectRatio="none"
+            shape-rendering="auto"
+          >
+            <defs>
+              <path
+                id="gentle-wave"
+                d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+              />
+            </defs>
+            <g class="parallax">
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="0"
+                fill="rgba(255,255,255,0.7"
+              />
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="3"
+                fill="rgba(255,255,255,0.5)"
+              />
+              <use
+                xlinkHref="#gentle-wave"
+                x="48"
+                y="5"
+                fill="rgba(255,255,255,0.3)"
+              />
+              <use xlinkHref="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+          </svg>
+        </div>
+        <div class="content flex">
+          <p>&copy; Donat Kapesa | 2019</p>
+        </div>
       </div>
     );
   }
